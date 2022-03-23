3ee0ee37fdfe1253727efeb6656e75d080b217f9
Bobby Aina <Babsaina@ymail.com>
2022-03-23 13:49:58 -0500

    ok

--- test/app-test.js
+++ test/app-test.js
@@ -2,6 +2,20 @@
 const {assert} = require('chai');
 const { describe, it } = require('mocha');
 const app = require('../app');
-
-
+const{isPerfectSquare} = require('../app')
+//setup
+// const isPrefectSquare = require('../app');
 // Write tests function below this line
+
+describe('isPerfectSquare',()=>{
+    
+    it('returns true if the number is a perfect square',()=>{
+        //setup
+        const num = 7;
+        const result  = false;
+        //excercise
+
+        //verify 
+        assert.ok(isPerfectSquare(num) === result);
+})
+})
